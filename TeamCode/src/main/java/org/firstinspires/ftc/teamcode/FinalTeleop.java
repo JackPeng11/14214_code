4a179bcc0faa77bc9ba406616a9f61e86cee983d
jack <jackpeng3545@gmail.com>
2021-02-20 14:58:34 -0800

    tuning shooting velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleop.java
@@ -138,7 +138,7 @@ public final class FinalTeleop extends LinearOpMode
             }
             else
             {
-                setVelocity(flyWheel, 0.2);
+                setVelocity(flyWheel, 0.1);
             }
             ++toggleRingFlowCount;
             
