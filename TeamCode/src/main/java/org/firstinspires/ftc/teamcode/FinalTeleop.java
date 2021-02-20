34ca4903ceb7e7a63e28a3f75c08f714b089ebc2
Aryan <voraaryan1@gmail.com>
2021-02-20 14:20:34 -0800

    changed power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleop.java
@@ -174,7 +174,7 @@ public final class FinalTeleop extends LinearOpMode
             
             if (gamepad2.left_bumper)
             {
-                setVelocity(spinner, 0.64);
+                setVelocity(spinner, 0.50);
             }
         }
         else
